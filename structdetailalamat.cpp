59749d923714c4530103d3b2ba316f0dc48ed491
aldys002 <igidiaaldys@gmail.com>
2024-04-02 13:12:49 +0700

    menambah titik koma setelah kalimat string nama

--- structdetailalamat.cpp
+++ structdetailalamat.cpp
@@ -8,11 +8,32 @@ struct DetailAlamat {
 
 struct mahasiswa {
     string nim;
-    string nama
+    string nama;
     DetailAlamat alamat;
     int umur;
 };
 
 int main(){
-    
+    mahasiswa mhs;
+    cout << "nomor mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "alamat mahasiswa : ";
+    cout << "\t nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n alamat : ";
+    cout << "\n Desa : " << mhs.alamat.desa;
+    cout << "\n Kota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
 }
