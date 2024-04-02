903987e31b03078e0fefeab05f2757f0b6264c85
aldys002 <igidiaaldys@gmail.com>
2024-04-02 13:00:33 +0700

    membuat struct detail alamat

--- structmahasiswa.cpp
+++ structmahasiswa.cpp
@@ -9,5 +9,20 @@ struct mahasiswa {
 };
 
 int main(){
+    mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM :" << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
     
 }
