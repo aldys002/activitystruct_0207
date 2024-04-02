08d17c22a1b6256f5218cfd184dc1e12eeef2fff
aldys002 <igidiaaldys@gmail.com>
2024-04-02 13:39:20 +0700

    merubah huruf n dan m pertama menjadi kapital pada kalimat nomor mahasiswa

--- 3.cpp
+++ 3.cpp
@@ -14,5 +14,25 @@ struct mahasiswa {
 };
 
 int main (){
+    cout << "Nomor Mahasiswa : "; 
+    getline (cin, mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline (cin, mhs.nama);
+
+    cout << "alamat mahasiswa : "<< endl;
+    cout << "\t nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "umjur mahasiswa : ";
+    cin >> mhs.umjur;
+
+    cout << endl;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n alamat : ";
+    cout << "\n \t Desa : " << mhs.alamat.desa;
+    cout << "\n \t Kota : "mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
+
     
 }
